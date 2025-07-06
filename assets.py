50e296a4effb6d1dea32fec5c561fb01855122e7
Nimira43 <n.rabone72@aol.com>
2025-07-06 04:28:20 +0100

    Completed load_sprite_range function for the time being.

--- assets.py
+++ assets.py
@@ -20,4 +20,12 @@ class Assets:
     return image
 
   def load_sprite_range(self, image_dict, spritesheet, row=gs.SIZE, col=gs.SIZE, width=gs.SIZE, height=gs.SIZE, resize=False):
-    pass
+    animation_images = {}
+    for animation in image_dict.keys():
+      animation_images[animation] = []
+      for coord in image_dict[animation]:
+        image = self.load_sprites(spritesheet, coord[1] * col, coord[0] * row, width, height)
+        if resize:
+          image = pygame.transform.scale(image, (32, 32))
+        animation_images[animation].append(image)
+    return animation_images
