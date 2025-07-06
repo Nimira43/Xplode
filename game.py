6419bacf68d4106b985e563c0cd7252401b1b9fd
Nimira43 <n.rabone72@aol.com>
2025-07-06 03:47:14 +0100

    Updated input in Game

--- game.py
+++ game.py
@@ -6,14 +6,10 @@ class Game:
   def __init__(self, main, assets):
     self.MAIN = main
     self.ASSETS = assets
+    self.player = Character(self)
 
   def input(self):
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        self.MAIN.run = False
-      elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
-          self.MAIN.run = False
+    self.player.input()
 
   def update(self):
     pass
