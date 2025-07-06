c43eca3a0f6360a8bc214d6e4b7a056be75095ce
Nimira43 <n.rabone72@aol.com>
2025-07-06 05:50:09 +0100

    Created more variables inside gamesetting.py

--- gamesettings.py
+++ gamesettings.py
@@ -1,6 +1,8 @@
 SCREENWIDTH = 1280
 SCREENHEIGHT = 892
 FPS = 60
+Y_OFFSET = 92
+
 SIZE = 64
 ROWS = 12
 COLS = 30
@@ -9,9 +11,14 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+GREY = (188, 188, 188)
 
 PLAYER = {'walk_left': [(0, 1), (0, 0), (0, 2)],
           'walk_down': [(0, 4), (0, 3), (0, 5)],
           'walk_right': [(0, 7), (0, 6), (0, 8)],
           'walk_up': [(0, 10), (0, 9), (0, 11)],
           'dead_anim': [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)]}
+
+HARD_BLOCK = {'hard_block': [(1, 10)]}
+SOFT_BLOCK = {'soft_block': [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6)]}
+BACKGROUND = {'background': [(2, 11)]}
