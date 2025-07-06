fb116d7861dffe60fccdb680eaa24fe193a64c2f
Nimira43 <n.rabone72@aol.com>
2025-07-06 04:10:25 +0100

    Update Assets init

--- assets.py
+++ assets.py
@@ -5,6 +5,8 @@ class Assets:
   def __init__(self):
     self.spritesheet = self.load_sprite_sheet('image', 'spritesheet.png', 192 * 4, 272 * 4)
 
+    self.player_char = self.load_sprite_range(gs.PLAYER, self.spritesheet)
+
   def load_sprite_sheet(self, path, filename, width, height):
     image = pygame.image.load(f'{path}/{filename}').convert_alpha()
     image = pygame.transform.scale(image, (width, height))
