eb9e2491ad41781e3564b237c5c4b26cd3ec463a
Nimira43 <n.rabone72@aol.com>
2025-07-06 03:16:19 +0100

    load_sprite_sheet function

--- assets.py
+++ assets.py
@@ -4,3 +4,8 @@ import gamesettings as gs
 class Assets:
   def __init__(self):
     self.spritesheet = self.load_sprite_sheet('image', 'spritesheet.png', 192 * 4, 272 * 4)
+
+  def load_sprite_sheet(self, path, filename, width, height):
+    image = pygame.image.load(f'{path}/{filename}').convert_alpha()
+    image = pygame.transform.scale(image, (width, height))
+    return image
