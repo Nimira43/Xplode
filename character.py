952de857621f2e26d2bb33748448af244abd27f9
Nimira43 <n.rabone72@aol.com>
2025-07-06 03:43:54 +0100

    Updated input function

--- character.py
+++ character.py
@@ -12,7 +12,12 @@ class Character(pygame.sprite.Sprite):
     self.rect = pygame.Rect(self.x, self.y, gs.SIZE, gs.SIZE)
 
   def input(self):
-    pass
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        self.GAME.MAIN.run = False
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+          self.GAME.MAIN.run = False
 
   def update(self):
     pass
