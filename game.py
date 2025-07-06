9fc6576a9de7a39c3f262fc8655f7e309b0d5167
Nimira43 <n.rabone72@aol.com>
2025-07-06 05:31:24 +0100

    Updated game.py

--- game.py
+++ game.py
@@ -6,7 +6,10 @@ class Game:
   def __init__(self, main, assets):
     self.MAIN = main
     self.ASSETS = assets
-    self.player = Character(self, self.ASSETS)
+    self.player = Character(self, self.ASSETS, player_char)
+
+    self.level = 1
+    self.level_matrix = self.generate_level_matrix(gs.ROWS, gs.COLS)
 
   def input(self):
     self.player.input()
@@ -16,3 +19,14 @@ class Game:
 
   def draw(self, window):
     self.player.draw(window)
+
+  def generate_level_matrix(self, rows, cols):
+    matrix = []
+    for row in range(rows + 1):
+      line = []
+      for col in range(cols + 1):
+        line.append('_')
+      matrix.append(line)
+    for row in matrix:
+      print(row)
+    return matrix
