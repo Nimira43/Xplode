db2db7c52a402558bb6f232225b35faabb24d318
Nimira43 <n.rabone72@aol.com>
2025-07-06 05:08:28 +0100

    Updated

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ class Game:
   def __init__(self, main, assets):
     self.MAIN = main
     self.ASSETS = assets
-    self.player = Character(self, self.ASSETS, player_char)
+    self.player = Character(self, self.ASSETS)
 
   def input(self):
     self.player.input()
