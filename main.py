6810b1e552a8af420c9740a1b5d05d0f33c02552
Nimira43 <n.rabone72@aol.com>
2025-07-06 01:49:27 +0100

    Defining Xplode class with empty functions

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
 import gamesettings as gs
 
+class Xplode:
+  def __init__(self):
+    pass
+
+  def input(self):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
+
+  def rungame(self):
+    pass
