4274c14b460970af8b331ea514ae6ccdc8c050b5
Nimira43 <n.rabone72@aol.com>
2025-07-06 03:38:37 +0100

    Created outline for Character class

---
+++ character.py
@@ -0,0 +1,15 @@
+import pygame
+import gamesettings as gs
+
+class Character(pygame.sprite.Sprite):
+  def __init__(self, game):
+    pass
+
+  def input(self):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
