124e954932589874fc32e1bf28330779407d2633
Nimira43 <n.rabone72@aol.com>
2025-07-06 03:24:53 +0100

    Setting up outline of Game class

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+import gamesettings as gs
+
+class Game:
+  def __init__(self, main, assets):
+    pass
+
+  def input(self):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
