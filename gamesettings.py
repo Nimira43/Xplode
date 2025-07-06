e0d8feb1d61445daf00969bdb7bab1d31f8bc501
Nimira43 <n.rabone72@aol.com>
2025-07-06 04:05:18 +0100

    Creating player dictionary

--- gamesettings.py
+++ gamesettings.py
@@ -2,7 +2,16 @@ SCREENWIDTH = 1280
 SCREENHEIGHT = 892
 FPS = 60
 SIZE = 64
+
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+PLAYER = {
+  'walk_left': [(0, 1), (0, 0), (0, 2)],
+  'walk_down': [(0, 1), (0, 0), (0, 2)],
+  'walk_right': [(0, 1), (0, 0), (0, 2)],
+  'walk_up': [(0, 1), (0, 0), (0, 2)],
+  'dead_anim': [(0, 1), (0, 0), (0, 2)],
+}
