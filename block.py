0964abb808c5eaf92913754ec191c1ede05b7e5a
Nimira43 <n.rabone72@aol.com>
2025-07-06 06:02:52 +0100

    Setting up Blocks outline

---
+++ block.py
@@ -0,0 +1,15 @@
+import pygame
+import gamesettings as gs
+
+class Blocks(pygame.sprite.Sprite):
+  def __init__(self, game, images, group, row_num, col_num, size):
+    pass
+
+  def update(self):
+    pass
+
+  def draw(self, window):
+    pass
+
+  def __repr__(self):
+    pass
