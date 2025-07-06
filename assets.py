b89981557258887d8bed78916afd9bc529acfeeb
Nimira43 <n.rabone72@aol.com>
2025-07-06 05:19:37 +0100

    Updated assets.py

--- assets.py
+++ assets.py
@@ -3,12 +3,12 @@ import gamesettings as gs
 
 class Assets:
   def __init__(self):
-    self.spritesheet = self.load_sprite_sheet('image', 'spritesheet.png', 192 * 4, 272 * 4)
+    self.spritesheet = self.load_sprite_sheet('images', 'spritesheet.png', 192 * 4, 272 * 4)
 
     self.player_char = self.load_sprite_range(gs.PLAYER, self.spritesheet)
 
   def load_sprite_sheet(self, path, filename, width, height):
-    image = pygame.image.load(f'{path}/{filename}').convert_alpha()
+    image = pygame.image.load(f"{path}/{filename}").convert_alpha()
     image = pygame.transform.scale(image, (width, height))
     return image
   
