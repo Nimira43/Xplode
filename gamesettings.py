cddb8990d43c8959c6a7526c498e43aa765ebbad
Nimira43 <n.rabone72@aol.com>
2025-07-10 04:19:44 +0100

    Bomb dictionary

--- gamesettings.py
+++ gamesettings.py
@@ -22,3 +22,4 @@ PLAYER = {'walk_left': [(0, 1), (0, 0), (0, 2)],
 HARD_BLOCK = {'hard_block': [(1, 10)]}
 SOFT_BLOCK = {'soft_block': [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6)]}
 BACKGROUND = {'background': [(2, 11)]}
+BOMB = {'bomb': [(1, 7), (1, 8), (1, 9), (1, 8)]}
