8393169c18e7300f9eb123be4112ef5907548989
Nimira43 <n.rabone72@aol.com>
2025-07-31 20:38:50 +0100

    init - 1

--- enemy.py
+++ enemy.py
@@ -5,6 +5,13 @@ from random import choice
 class Enemy(pygame.sprite.Sprite):
   def __init__(self, game, image_dict, group, row_num, col_num, size):
     super().__init__(group)
+    self.GAME = game
+    self.speed = 1
+    self.wall_hack = False
+    self.chase_player = False
+    self.LoS = 0
+    self.see_player_hack = False
+    
 
   def update(self):
     pass
