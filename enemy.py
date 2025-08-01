ae651f56b47a76bd23d45fadcfe4ee8072172255
Nimira43 <n.rabone72@aol.com>
2025-08-01 20:09:36 +0100

    init - 4

--- enemy.py
+++ enemy.py
@@ -20,6 +20,14 @@ class Enemy(pygame.sprite.Sprite):
     self.direction = 'left'
     self.dir_mvmt = {'left': -self.speed, 'right': self.speed, 'up': -self.speed, 'down': self.speed}
 
+    self.change_dir_timer = pygame.time.get_ticks()
+    self.dir_time = 1500
+    self.index = 0
+    self.action = f'walk_{self.direction}'
+    self.image_dict = image_dict
+    self.anim_frame_time = 100
+    self.anim_timer = pygame.time.get_ticks()
+
   def update(self):
     pass
 
