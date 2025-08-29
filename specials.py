a3d4f84d72b027cc1b1ff2815e744d4bd76a6a9b
Nimira43 <n.rabone72@aol.com>
2025-08-29 04:01:13 +0100

    Parameters passed into methods

--- specials.py
+++ specials.py
@@ -2,42 +2,43 @@ import pygame
 import gamesettings as gc
 
 class Special(pygame.sprite.Sprite):
-  def __init__():
+  def __init__(self, game, image, name, group, row_num, col_num, size):
+    super().__init__(group)
     pass
 
-  def update():
+  def update(self):
     pass
 
-  def draw():
+  def draw(self, window, x_offset):
     pass
 
-  def bomb_up_special(): 
+  def bomb_up_special(self, player): 
     pass
 
-  def fire_up_special():
+  def fire_up_special(self, player):
     pass
   
-  def speed_up_special():
+  def speed_up_special(self, player):
     pass
   
-  def wall_hack_special():
+  def wall_hack_special(self, player):
     pass
   
-  def remote_special():
+  def remote_special(self, player):
     pass
   
-  def bomb_hack_special():
+  def bomb_hack_special(self, player):
     pass
   
-  def flame_pass_special():
+  def flame_pass_special(self, player):
     pass
   
-  def invincible_special():
+  def invincible_special(self, player):
     pass
   
-  def end_stage():
+  def end_stage(self, player):
     pass
   
-  def hit_by_explosion():
+  def hit_by_explosion(self):
     pass
   
