04709748283092b89c5d49750e70cc3a42b2ea52
Nimira43 <n.rabone72@aol.com>
2025-08-29 03:56:55 +0100

    Outling Special class and methods

--- specials.py
+++ specials.py
@@ -1,3 +1,43 @@
 import pygame
 import gamesettings as gc
 
+class Special(pygame.sprite.Sprite):
+  def __init__():
+    pass
+
+  def update():
+    pass
+
+  def draw():
+    pass
+
+  def bomb_up_special(): 
+    pass
+
+  def fire_up_special():
+    pass
+  
+  def speed_up_special():
+    pass
+  
+  def wall_hack_special():
+    pass
+  
+  def remote_special():
+    pass
+  
+  def bomb_hack_special():
+    pass
+  
+  def flame_pass_special():
+    pass
+  
+  def invincible_special():
+    pass
+  
+  def end_stage():
+    pass
+  
+  def hit_by_explosion():
+    pass
+  
